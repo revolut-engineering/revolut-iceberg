szehon-ho/iceberg

Internal: Disable locality by default

spark2/src/main/java/org/apache/iceberg/spark/source/Reader.java
@@ -104,6 +104,7 @@ class Reader implements DataSourceReader, SupportsScanColumnarBatch, SupportsPus
   private List<CombinedScanTask> tasks = null; // lazy cache of tasks
   private Boolean readUsingBatch = null;
 
+  @SuppressWarnings("checkstyle:CyclomaticComplexity")
   Reader(Table table, Broadcast<FileIO> io, Broadcast<EncryptionManager> encryptionManager,
       boolean caseSensitive, DataSourceOptions options) {
     this.table = table;
@@ -133,8 +134,9 @@ class Reader implements DataSourceReader, SupportsScanColumnarBatch, SupportsPus
     this.splitLookback = options.get(SparkReadOptions.LOOKBACK).map(Integer::parseInt).orElse(null);
     this.splitOpenFileCost = options.get(SparkReadOptions.FILE_OPEN_COST).map(Long::parseLong).orElse(null);
 
-    if (io.getValue() instanceof HadoopFileIO) {
-      String fsscheme = "no_exist";
+    boolean localityOption = options.get("locality").map(Boolean::parseBoolean).orElse(false);
+    if (localityOption && io.getValue() instanceof HadoopFileIO) {
+      String scheme = "no_exist";
       try {
         Configuration conf = SparkSession.active().sessionState().newHadoopConf();
         // merge hadoop config set on table
@@ -142,13 +144,11 @@ class Reader implements DataSourceReader, SupportsScanColumnarBatch, SupportsPus
         // merge hadoop config passed as options and overwrite the one on table
         mergeIcebergHadoopConfs(conf, options.asMap());
         FileSystem fs = new Path(table.location()).getFileSystem(conf);
-        fsscheme = fs.getScheme().toLowerCase(Locale.ENGLISH);
+        scheme = fs.getScheme().toLowerCase(Locale.ENGLISH);
       } catch (IOException ioe) {
         LOG.warn("Failed to get Hadoop Filesystem", ioe);
       }
-      String scheme = fsscheme; // Makes an effectively final version of scheme
-      this.localityPreferred = options.get("locality").map(Boolean::parseBoolean)
-          .orElseGet(() -> LOCALITY_WHITELIST_FS.contains(scheme));
+      this.localityPreferred = LOCALITY_WHITELIST_FS.contains(scheme);
     } else {
       this.localityPreferred = false;
     }

revolut-engineering/revolut-iceberg

spark2/src/main/java/org/apache/iceberg/spark/source/Reader.java
@@ -107,6 +107,7 @@ class Reader implements DataSourceReader, SupportsScanColumnarBatch, SupportsPus
   private Boolean readUsingBatch = null;
   private int batchSize = 0;
 
+  @SuppressWarnings("checkstyle:CyclomaticComplexity")
   Reader(SparkSession spark, Table table, boolean caseSensitive, DataSourceOptions options) {
     this.sparkContext = JavaSparkContext.fromSparkContext(spark.sparkContext());
     this.table = table;
@@ -137,8 +138,9 @@ class Reader implements DataSourceReader, SupportsScanColumnarBatch, SupportsPus
     this.splitLookback = options.get(SparkReadOptions.LOOKBACK).map(Integer::parseInt).orElse(null);
     this.splitOpenFileCost = options.get(SparkReadOptions.FILE_OPEN_COST).map(Long::parseLong).orElse(null);
 
-    if (table.io() instanceof HadoopFileIO) {
-      String fsscheme = "no_exist";
+    boolean localityOption = options.get("locality").map(Boolean::parseBoolean).orElse(false);
+    if (localityOption && table.io() instanceof HadoopFileIO) {
+      String scheme = "no_exist";
       try {
         Configuration conf = SparkSession.active().sessionState().newHadoopConf();
         // merge hadoop config set on table
@@ -146,13 +148,11 @@ class Reader implements DataSourceReader, SupportsScanColumnarBatch, SupportsPus
         // merge hadoop config passed as options and overwrite the one on table
         mergeIcebergHadoopConfs(conf, options.asMap());
         FileSystem fs = new Path(table.location()).getFileSystem(conf);
-        fsscheme = fs.getScheme().toLowerCase(Locale.ENGLISH);
+        scheme = fs.getScheme().toLowerCase(Locale.ENGLISH);
       } catch (IOException ioe) {
         LOG.warn("Failed to get Hadoop Filesystem", ioe);
       }
-      String scheme = fsscheme; // Makes an effectively final version of scheme
-      this.localityPreferred = options.get("locality").map(Boolean::parseBoolean)
-          .orElseGet(() -> LOCALITY_WHITELIST_FS.contains(scheme));
+      this.localityPreferred = LOCALITY_WHITELIST_FS.contains(scheme);
     } else {
       this.localityPreferred = false;
     }